de1d3335503de84e9222f74e55d8b9bf11ac4893
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2026-07-21 12:44:09 +0200

    Add strided and channel convolutions

---
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Perform convolution on images containing channels."""
+
+import numpy as np
+
+
+def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
+    """Convolve multi-channel images with one kernel."""
+    m, h, w, c = images.shape
+    kh, kw, kc = kernel.shape
+    sh, sw = stride
+    if padding == 'same':
+        oh = int(np.ceil(h / sh))
+        ow = int(np.ceil(w / sw))
+        total_h = max((oh - 1) * sh + kh - h, 0)
+        total_w = max((ow - 1) * sw + kw - w, 0)
+        ph_before = total_h // 2
+        pw_before = total_w // 2
+        pad_width = ((0, 0), (ph_before, total_h - ph_before),
+                     (pw_before, total_w - pw_before), (0, 0))
+    else:
+        if padding == 'valid':
+            ph, pw = 0, 0
+        else:
+            ph, pw = padding
+        pad_width = ((0, 0), (ph, ph), (pw, pw), (0, 0))
+        oh = (h + (2 * ph) - kh) // sh + 1
+        ow = (w + (2 * pw) - kw) // sw + 1
+    padded = np.pad(images, pad_width, mode='constant')
+    output = np.zeros((m, oh, ow))
+    for i in range(oh):
+        for j in range(ow):
+            row = i * sh
+            column = j * sw
+            region = padded[:, row:row + kh, column:column + kw, :]
+            output[:, i, j] = np.sum(region * kernel, axis=(1, 2, 3))
+    return output
