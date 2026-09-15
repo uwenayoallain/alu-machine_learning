1ebe3ff37dbf9d5db90af546f2e0eed7873b4bf3
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2026-09-15 14:00:45 +0200

    task 23 fix

--- supervised_learning/classification/23-deep_neural_network.py
+++ supervised_learning/classification/23-deep_neural_network.py
@@ -2,9 +2,9 @@
 """Adds progress reporting and plotting to a deep network."""
 
 import matplotlib.pyplot as plt
-from importlib import import_module
-
-_Base = import_module('22-deep_neural_network').DeepNeuralNetwork
+_namespace = {}
+exec(open('22-deep_neural_network.py').read(), _namespace)
+_Base = _namespace['DeepNeuralNetwork']
 
 
 class DeepNeuralNetwork(_Base):
