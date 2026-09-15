296c672237df2ea43793fc2ab70ebf7cff2dbd9d
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2026-09-15 12:58:11 +0200

    task 1

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Defines a private single neuron."""
+
+import numpy as np
+
+
+class Neuron:
+    """Represents a neuron with read-only public properties."""
+
+    def __init__(self, nx):
+        """Initialize the neuron."""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Return the weights."""
+        return self.__W
+
+    @property
+    def b(self):
+        """Return the bias."""
+        return self.__b
+
+    @property
+    def A(self):
+        """Return the activated output."""
+        return self.__A
