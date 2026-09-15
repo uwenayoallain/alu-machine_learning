5eb38e50f34f67da6b0d6511cc76d7d4bd66fce6
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2026-09-15 14:25:14 +0200

    task 7 style

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -15,5 +15,5 @@ def evaluate(X, Y, save_path):
         y_pred = graph.get_collection('y_pred')[0]
         loss = graph.get_collection('loss')[0]
         accuracy = graph.get_collection('accuracy')[0]
-        return session.run((y_pred, accuracy, loss),
-                          feed_dict={x: X, y: Y})
+        return session.run(
+            (y_pred, accuracy, loss), feed_dict={x: X, y: Y})
