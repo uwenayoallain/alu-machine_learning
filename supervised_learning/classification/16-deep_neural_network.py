0c5d06d80068b2f52f139ff03e97d39803c67812
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2026-09-15 14:17:27 +0200

    task 16 style

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -22,7 +22,7 @@ class DeepNeuralNetwork:
         for layer, nodes in enumerate(layers, 1):
             if type(nodes) is not int or nodes <= 0:
                 raise TypeError("layers must be a list of positive integers")
-            self.weights['W{}'.format(layer)] = (np.random.randn(nodes,
-                previous) * np.sqrt(2 / previous))
+            self.weights['W{}'.format(layer)] = (
+                np.random.randn(nodes, previous) * np.sqrt(2 / previous))
             self.weights['b{}'.format(layer)] = np.zeros((nodes, 1))
             previous = nodes
