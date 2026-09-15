77336e0d428db549ae1eb279733d53ca220a370e
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2026-09-15 14:12:07 +0200

    task 14 fix

--- supervised_learning/classification/14-neural_network.py
+++ supervised_learning/classification/14-neural_network.py
@@ -72,6 +72,20 @@ class NeuralNetwork:
         _, A = self.forward_prop(X)
         return np.where(A >= 0.5, 1, 0), self.cost(Y, A)
 
+    def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
+        """Perform one backpropagation update."""
+        m = Y.shape[1]
+        dz2 = A2 - Y
+        dw2 = np.matmul(dz2, A1.T) / m
+        db2 = np.sum(dz2, axis=1, keepdims=True) / m
+        dz1 = np.matmul(self.__W2.T, dz2) * A1 * (1 - A1)
+        dw1 = np.matmul(dz1, X.T) / m
+        db1 = np.sum(dz1, axis=1, keepdims=True) / m
+        self.__W1 -= alpha * dw1
+        self.__b1 -= alpha * db1
+        self.__W2 -= alpha * dw2
+        self.__b2 -= alpha * db2
+
     def train(self, X, Y, iterations=5000, alpha=0.05):
         """Train and evaluate the network."""
         if not isinstance(iterations, int):
