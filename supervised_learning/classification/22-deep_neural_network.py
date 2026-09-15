39fff767e7d9036425504ff959da850ebd8892b7
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2026-09-15 12:58:13 +0200

    task 22

---
+++ supervised_learning/classification/22-deep_neural_network.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Adds training to a deep network."""
+
+from importlib import import_module
+
+_Base = import_module('21-deep_neural_network').DeepNeuralNetwork
+
+
+class DeepNeuralNetwork(_Base):
+    """A trainable deep sigmoid network."""
+
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """Train and evaluate the network."""
+        if not isinstance(iterations, int):
+            raise TypeError("iterations must be an integer")
+        if iterations < 1:
+            raise ValueError("iterations must be a positive integer")
+        if not isinstance(alpha, float):
+            raise TypeError("alpha must be a float")
+        if alpha <= 0:
+            raise ValueError("alpha must be positive")
+        for _ in range(iterations):
+            A, cache = self.forward_prop(X)
+            self.gradient_descent(Y, cache, alpha)
+        return self.evaluate(X, Y)
