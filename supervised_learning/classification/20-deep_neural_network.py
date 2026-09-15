1ecb9c14328cb6dd97c285f5b68cdf0fa702935b
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2026-09-15 14:00:45 +0200

    task 20 fix

--- supervised_learning/classification/20-deep_neural_network.py
+++ supervised_learning/classification/20-deep_neural_network.py
@@ -2,9 +2,9 @@
 """Adds evaluation to a deep network."""
 
 import numpy as np
-from importlib import import_module
-
-_Base = import_module('19-deep_neural_network').DeepNeuralNetwork
+_namespace = {}
+exec(open('19-deep_neural_network.py').read(), _namespace)
+_Base = _namespace['DeepNeuralNetwork']
 
 
 class DeepNeuralNetwork(_Base):
