fc71322695f7027059ece43286412461057d8091
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2026-09-25 02:15:29 +0200

    task 6 dropout layer

---
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Create a TensorFlow dense layer followed by dropout."""
+
+import tensorflow as tf
+
+
+def dropout_create_layer(prev, n, activation, keep_prob):
+    """Return the output of a variance-scaled dense layer with dropout."""
+    initializer = tf.contrib.layers.variance_scaling_initializer(
+        mode="FAN_AVG")
+    layer = tf.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=initializer)
+    dropout = tf.layers.Dropout(rate=1 - keep_prob)
+    return dropout(layer(prev), training=True)
